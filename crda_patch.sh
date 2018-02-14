1041a6de720f252e94ab52f1732cc7fdbf740ad6
root <root@localhost.localdomain>
2018-02-14 03:33:24 +0300

    add crda_patch.sh

---
+++ crda_patch.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+
+apt-get update && apt-get install -y python-m2crypto libgcrypt20 libgcrypt20-dev git gcc libnl-genl-3-dev
+
+cd /tmp
+git clone https://github.com/donyshow/wireless.git
+cd wireless/
+make && cp -f regulatory.bin /lib/crda/regulatory.bin
+
+cp root.key.pub.pem /lib/crda/pubkeys/
+cd /tmp	
+rm -r wireless
+echo "DONE"
